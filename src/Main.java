6f841e5182a2977776e18a04db886af6b1cb6398
c-p-30 <pc0269395@otc.edu>
2025-01-23 12:48:59 -0600

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(1, 2));
+        System.out.println(calc.sub(1, 2));
+        System.out.println(calc.mul(1, 2));
+        System.out.println(calc.div(1, 2));
+
+        @Test
+        void testAdd;{
+            Assert.assertEquals(21, Calculator.add(7,14 ) ) ;
+        }
+    }
+}
