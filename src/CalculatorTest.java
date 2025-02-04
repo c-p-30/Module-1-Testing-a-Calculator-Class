f4307746b710906d820727184ebd23eb7e70c18d
COFFELT, PAUL K. <pc0269395@otc.edu>
2025-02-03 23:12:26 -0600

    Final(Final) Changes

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -5,21 +5,21 @@ class CalculatorTest {
     @org.junit.jupiter.api.Test
     void add() {
         assertEquals(3, Calculator.add(1, 2));
-        assertEquals(-1, Calculator.add(1,-2));
+        assertEquals(-3, Calculator.add(-1,-2));
         assertEquals(0, Calculator.add(1,-1));
     }
 
     @org.junit.jupiter.api.Test
     void sub() {
         assertEquals(3, Calculator.sub(5, 2));
-        assertEquals(3, Calculator.sub(1,-2));
+        assertEquals(1, Calculator.sub(-1,-2));
         assertEquals(0, Calculator.sub(1,1));
     }
 
     @org.junit.jupiter.api.Test
     void mul() {
         assertEquals(2, Calculator.mul(1, 2));
-        assertEquals(-2, Calculator.mul(1,-2));
+        assertEquals(2, Calculator.mul(-1,-2));
         assertEquals(0, Calculator.mul(1,0));
     }
 
