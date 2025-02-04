fcfced2156c599a6baf7ec5c6f24f1cf9b605642
COFFELT, PAUL K. <pc0269395@otc.edu>
2025-02-03 23:06:22 -0600

    Final Changes

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,6 +10,9 @@ public class Calculator {
         return a * b;
     }
     public static double div(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Dividing by zero");
+        }
         return (double) a / b;
     }
 }
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -4,17 +4,34 @@ class CalculatorTest {
 
     @org.junit.jupiter.api.Test
     void add() {
+        assertEquals(3, Calculator.add(1, 2));
+        assertEquals(-1, Calculator.add(1,-2));
+        assertEquals(0, Calculator.add(1,-1));
     }
 
     @org.junit.jupiter.api.Test
     void sub() {
+        assertEquals(3, Calculator.sub(5, 2));
+        assertEquals(3, Calculator.sub(1,-2));
+        assertEquals(0, Calculator.sub(1,1));
     }
 
     @org.junit.jupiter.api.Test
     void mul() {
+        assertEquals(2, Calculator.mul(1, 2));
+        assertEquals(-2, Calculator.mul(1,-2));
+        assertEquals(0, Calculator.mul(1,0));
     }
 
     @org.junit.jupiter.api.Test
     void div() {
+        assertEquals(2.5, Calculator.div(5, 2));
+        assertEquals(-0.5, Calculator.div(1,-2));
+
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            Calculator.div(8,0);
+        });
+
+        assertEquals("Dividing by zero", exception.getMessage());
     }
 }
